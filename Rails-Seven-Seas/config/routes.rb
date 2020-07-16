54dffafe78717f22ec4d99f083b1f9dd196f94c0
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-07-16 19:10:22 -0400

    Done.

--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+    #HTTPVERB 'ROUTE', to: 'controller#method'
+    get '/', to: 'seas#welcome' 
+    get 'seas', to: 'seas#index'
+    
+    get 'seas/new', to: 'seas#new'
+    post 'seas/', to: 'seas#create' 
+    
+    get 'seas/:id', to: 'seas#show', as: 'sea'
+    get 'seas/:id/edit', to: 'seas#edit'
+    patch 'seas/:id', to: 'seas#patch'
 end
